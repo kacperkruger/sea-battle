7c39f3a8d0b1056d9885d830ea9d6e5f4c8119b1
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-20 19:56:45 +0200

    show, eq instances for player

--- src/main/scala/player/domain/Player.scala
+++ src/main/scala/player/domain/Player.scala
@@ -1,6 +1,13 @@
 package player.domain
 
+import cats.Show
+
 trait Player
 
 case object Player1 extends Player
 case object Player2 extends Player
+
+object Player {
+  given Show[Player] = Show.fromToString
+  given Eq[Player] =
+}
