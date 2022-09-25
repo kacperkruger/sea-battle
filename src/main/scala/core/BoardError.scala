bbc62f6eacb623e13fd33af5379e146da6550d0c
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-25 02:18:58 +0200

    board errors

---
+++ src/main/scala/core/BoardError.scala
@@ -0,0 +1,13 @@
+package core
+
+sealed trait BoardError {
+  def errorMessages: String
+}
+
+object CoordinateOutOfOrder extends BoardError {
+  override def errorMessages: String = "Yours coordinates aim out of board"
+}
+
+object CanNotPlaceShip extends BoardError {
+  override def errorMessages: String = "It is impossible to place sip there"
+}
