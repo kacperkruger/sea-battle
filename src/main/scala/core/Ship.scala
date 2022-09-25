ff394e811277ee8456075f9bbbf10a9677378267
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-25 02:19:29 +0200

    ships adt

---
+++ src/main/scala/core/Ship.scala
@@ -0,0 +1,33 @@
+package core
+
+import cats.syntax.show
+import cats.{Semigroup, Show}
+import core.Ship.{Battleship, Carrier}
+
+trait Ship {
+  val size: Int
+}
+object Ship {
+  object Carrier extends Ship {
+    override val size: Int = 5
+  }
+  object Battleship extends Ship {
+    override val size: Int = 4
+  }
+  object Submarine extends Ship {
+    override val size: Int = 3
+  }
+  object Destroyer extends Ship {
+    override val size: Int = 2
+  }
+
+  given Show[Ship] with {
+    def show(ship: Ship): String = ship match {
+      case Battleship => s"Battleship (${Battleship.size})"
+      case Carrier    => s"Carrier (${Carrier.size})"
+      case Destroyer  => s"Destroyer (${Destroyer.size})"
+      case Submarine  => s"Submarine (${Submarine.size})"
+      case _          => "Unknown ship"
+    }
+  }
+}
