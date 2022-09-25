814d41b0b5d63518bc7f4aba671f194cab64465c
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-25 02:19:11 +0200

    directions

---
+++ src/main/scala/core/Direction.scala
@@ -0,0 +1,9 @@
+package core
+
+trait Direction
+object Direction {
+  object Left extends Direction
+  object Right extends Direction
+  object Up extends Direction
+  object Down extends Direction
+}
