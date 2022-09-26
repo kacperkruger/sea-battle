19538a0b145a021e63dd02c1fc12d8ae3a8f8d80
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-26 15:04:06 +0200

    game errors

---
+++ src/main/scala/core/GameError.scala
@@ -0,0 +1,13 @@
+package core
+
+sealed trait GameError {
+  def errorMessage: String
+}
+
+object WrongPlayer extends GameError {
+  override def errorMessage: String = "Wrong player"
+}
+
+object GameIsOver extends GameError {
+  override def errorMessage: String = "Game has already ended"
+}
