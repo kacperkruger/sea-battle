f66b6eaf82e896a9f7c915d76e91a9ed5271cd46
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-28 23:32:27 +0200

    refactoring

--- src/main/scala/core/Board.scala
+++ src/main/scala/core/Board.scala
@@ -6,109 +6,126 @@ import core.Utilities.times
 
 import scala.annotation.tailrec
 
-case class Board(value: List[List[FieldStatus]])
+case class Board(value: List[List[FieldStatus]], player: Player)
 
 object Board {
-  def create: Board = {
+  def create(player: Player): Board = {
     Board(
       List.fill(BoardSize.size)(
         List.fill(BoardSize.size)(FieldStatus.Empty)
-      )
+      ),
+      player
     )
   }
 
-  extension (board: Board) {
-    def isCorrectCoordinate(coordinate: Coordinate): Boolean = {
-      import coordinate.*
-      x < BoardSize.size && x >= 0 && y < BoardSize.size && y >= 0
-    }
+  def apply(
+      board: Board,
+      coordinate: Coordinate
+  ): Either[BoardError, FieldStatus] = {
+    import coordinate.*
+    if (isCorrectCoordinate(coordinate)) {
+      Right(board.value(y)(x))
+    } else Left(CoordinateOutOfOrder)
+  }
 
-    def isEmpty(coordinate: Coordinate): Boolean =
-      apply(coordinate) == Right(FieldStatus.Empty)
+  def isCorrectCoordinate(coordinate: Coordinate): Boolean = {
+    import coordinate.*
+    x < BoardSize.size && x >= 0 && y < BoardSize.size && y >= 0
+  }
 
-    def apply(coordinate: Coordinate): Either[BoardError, FieldStatus] = {
-      import coordinate.*
-      if (isCorrectCoordinate(coordinate)) {
-        Right(board.value(y)(x))
-      } else Left(CoordinateOutOfOrder)
-    }
+  def isEmpty(board: Board, coordinate: Coordinate): Boolean =
+    Board.apply(board, coordinate) == Right(FieldStatus.Empty)
 
-    def update(
-        coordinate: Coordinate,
-        fieldStatus: FieldStatus
-    ): Either[BoardError, Board] = {
-      import coordinate.*
-      if (isCorrectCoordinate(coordinate)) {
-        val line = board.value(y)
-        Right(Board(board.value.updated(y, line.updated(x, fieldStatus))))
-      } else Left(CoordinateOutOfOrder)
-    }
-
-    def updateMultiply(
-        coordinates: List[Coordinate],
-        fieldStatus: FieldStatus
-    ): Either[BoardError, Board] =
-      coordinates.foldLeft[Either[BoardError, Board]](Right(board))(
-        (accEither, coordinate) =>
-          for {
-            acc <- accEither
-            updatedBoard <- acc.update(coordinate, fieldStatus)
-          } yield updatedBoard
-      )
-
-    @tailrec
-    def canPlaceShip(
-        coordinate: Coordinate,
-        size: Int,
-        direction: Direction,
-        acc: Boolean = true
-    ): Boolean = {
-      if (size == 0) acc
-      else if (!isEmpty(coordinate)) false
-      else
-        println(s"$coordinate")
-        canPlaceShip(
-          Coordinate.move(coordinate, direction),
-          size - 1,
-          direction
-        )
-    }
-
-    def numberOfShips: Int = board.value.map(_.count(_ == FieldStatus.Ship)).sum
-
-    def placeShip(
-        ship: Ship,
-        coordinate: Coordinate,
-        direction: Direction
-    ): Either[BoardError, Board] = {
-      if (
-        isCorrectCoordinate(coordinate) && canPlaceShip(
-          coordinate,
-          ship.size,
-          direction
+  def update(
+      board: Board,
+      coordinate: Coordinate,
+      fieldStatus: FieldStatus
+  ): Either[BoardError, Board] = {
+    import coordinate.*
+    if (Board.isCorrectCoordinate(coordinate)) {
+      val line = board.value(y)
+      Right(
+        Board(
+          board.value.updated(y, line.updated(x, fieldStatus)),
+          board.player
         )
       )
-        updateMultiply(
-          (0 until ship.size).toList
-            .map(n =>
-              times(coordinate, Coordinate.move(_: Coordinate, direction), n)
-            ),
-          FieldStatus.Ship
-        )
-      else Left(CanNotPlaceShip)
-    }
+    } else Left(CoordinateOutOfOrder)
+  }
 
-    def shipHit(coordinate: Coordinate): Boolean = {
-      apply(coordinate) == FieldStatus.Ship
-    }
+  def updateMultiply(
+      board: Board,
+      coordinates: List[Coordinate],
+      fieldStatus: FieldStatus
+  ): Either[BoardError, Board] = {
+    coordinates.foldLeft[Either[BoardError, Board]](Right(board))(
+      (accEither, coordinate) =>
+        for {
+          acc <- accEither
+          updatedBoard <- Board.update(acc, coordinate, fieldStatus)
+        } yield updatedBoard
+    )
+  }
 
-    def shoot(coordinate: Coordinate): Either[BoardError, Board] = {
-      if (!isCorrectCoordinate(coordinate)) Left(CoordinateOutOfOrder)
-      else if (isEmpty(coordinate)) update(coordinate, FieldStatus.MissedShot)
-      else if (apply(coordinate) == Right(FieldStatus.MissedShot))
-        Left(CanNotShootToShotField)
-      else update(coordinate, FieldStatus.DestroyedShip)
-    }
+  def numberOfShips(board: Board): Int = {
+    board.value.map(_.count(_ == FieldStatus.Ship)).sum
+  }
+
+  @tailrec
+  def canPlaceShip(
+      board: Board,
+      coordinate: Coordinate,
+      size: Int,
+      direction: Direction,
+      acc: Boolean = true
+  ): Boolean = {
+    if (size == 0) acc
+    else if (!Board.isEmpty(board, coordinate)) false
+    else
+      Board.canPlaceShip(
+        board,
+        Coordinate.move(coordinate, direction),
+        size - 1,
+        direction
+      )
+  }
+
+  def placeShip(
+      board: Board,
+      ship: Ship,
+      coordinate: Coordinate,
+      direction: Direction
+  ): Either[BoardError, Board] = {
+    if (
+      Board.isCorrectCoordinate(coordinate) && Board.canPlaceShip(
+        board,
+        coordinate,
+        ship.size,
+        direction
+      )
+    )
+      Board.updateMultiply(
+        board,
+        (0 until ship.size).toList
+          .map(n =>
+            times(coordinate, Coordinate.move(_: Coordinate, direction), n)
+          ),
+        FieldStatus.Ship
+      )
+    else Left(CanNotPlaceShip)
+  }
+
+  def shipHit(board: Board, coordinate: Coordinate): Boolean = {
+    Board.apply(board, coordinate) == FieldStatus.Ship
+  }
+
+  def shoot(board: Board, coordinate: Coordinate): Either[BoardError, Board] = {
+    if (!Board.isCorrectCoordinate(coordinate)) Left(CoordinateOutOfOrder)
+    else if (Board.isEmpty(board, coordinate))
+      Board.update(board, coordinate, FieldStatus.MissedShot)
+    else if (Board.apply(board, coordinate) == Right(FieldStatus.MissedShot))
+      Left(CanNotShootToShotField)
+    else Board.update(board, coordinate, FieldStatus.DestroyedShip)
   }
 
   given Show[Board] with {
