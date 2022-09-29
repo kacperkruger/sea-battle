694368c1ad1af808070c3888dcc4dc2438f26e6c
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-29 02:55:12 +0200

    refactoring

--- src/main/scala/core/GameError.scala
+++ src/main/scala/core/GameError.scala
@@ -1,25 +1,19 @@
 package core
 
-trait GameError {
+trait GameError:
   def errorMessage: String
-}
 
-object WrongPlayer extends GameError {
+object WrongPlayer extends GameError:
   override def errorMessage: String = "Wrong player"
-}
 
-object GameIsOver extends GameError {
+object GameIsOver extends GameError:
   override def errorMessage: String = "Game has already ended"
-}
 
-object CanNotPlaceDuringPlayPhase extends GameError {
+object CanNotPlaceDuringPlayPhase extends GameError:
   override def errorMessage: String = "Can't place a ship during playing phase"
-}
 
-object CanNotShootDuringPlacingPhase extends GameError {
+object CanNotShootDuringPlacingPhase extends GameError:
   override def errorMessage: String = "Can't shoot during placing phase"
-}
 
-object CanNotFindPlayer extends GameError {
+object CanNotFindPlayer extends GameError:
   override def errorMessage: String = "Can't find the player"
-}
