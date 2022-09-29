626129dfe931c9d41008b85b87168ba18e1e6ba3
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-29 02:57:04 +0200

    terminal console

---
+++ src/main/scala/core/LiveConsole.scala
@@ -0,0 +1,7 @@
+package core
+import cats.effect.IO
+
+object LiveConsole extends Console:
+  override def readLine: IO[String] = IO.readLine
+
+  override def printLine(s: String): IO[Unit] = IO.println(s)
