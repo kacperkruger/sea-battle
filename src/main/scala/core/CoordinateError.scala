709aad22a684cb8d05b40cd64c65e9b5f8cfd85a
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-29 02:53:26 +0200

    refactoring

--- src/main/scala/core/CoordinateError.scala
+++ src/main/scala/core/CoordinateError.scala
@@ -1,26 +1,19 @@
 package core
 
-sealed trait CoordinateError {
-  def errorMessages: String
-}
+sealed trait CoordinateError extends BoardError
 
-object FirstCoordinateOutOfRange extends CoordinateError {
-  override def errorMessages: String = "First coordinate out of range"
-}
+object FirstCoordinateOutOfRange extends CoordinateError:
+  override def errorMessage: String = "First coordinate out of range"
 
-object FirstCoordinateMustBeLetter extends CoordinateError {
-  override def errorMessages: String = "First coordinate must be letter"
-}
+object FirstCoordinateMustBeLetter extends CoordinateError:
+  override def errorMessage: String = "First coordinate must be letter"
 
-object SecondCoordinateOutOfRange extends CoordinateError {
-  override def errorMessages: String = "First coordinate out of range"
-}
+object SecondCoordinateOutOfRange extends CoordinateError:
+  override def errorMessage: String = "First coordinate out of range"
 
-object SecondCoordinateMustBeDigit extends CoordinateError {
-  override def errorMessages: String = "Second coordinate must be digit"
-}
+object SecondCoordinateMustBeDigit extends CoordinateError:
+  override def errorMessage: String = "Second coordinate must be digit"
 
-object InvalidLengthOfCoordinates extends CoordinateError {
-  override def errorMessages: String =
+object InvalidLengthOfCoordinates extends CoordinateError:
+  override def errorMessage: String =
     "Yours coordinates are too long or too short"
-}
