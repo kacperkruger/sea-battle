e50db06cf2d7262eed14ebfe39cbce59550565ee
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-29 02:57:25 +0200

    added name dependencie to player

--- src/main/scala/core/Player.scala
+++ src/main/scala/core/Player.scala
@@ -3,12 +3,13 @@ package core
 import cats.Show
 import cats.kernel.Eq
 
-trait Player
-
-case object Player1 extends Player
-case object Player2 extends Player
+case class Player(name: String)
 
 object Player {
-  given Show[Player] = Show.fromToString
+  def create(name: String): Player = Player(name)
+
+  given Show[Player] with {
+    def show(player: Player): String = player.name
+  }
   given Eq[Player] = Eq.fromUniversalEquals
 }
