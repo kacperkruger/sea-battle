0355593f06e12c063ddedf21be7e82be66839395
Kacper Kruger <kacper.kruger1@gmail.com>
2022-09-29 02:56:44 +0200

    added phase dependencies to game

--- src/main/scala/core/GameStatus.scala
+++ src/main/scala/core/GameStatus.scala
@@ -1,6 +1,8 @@
 package core
 
+import core.GamePhase
+
 trait GameStatus
 
-case class OnGoing(nextPlayer: Player) extends GameStatus
+case class OnGoing(board: Board, phase: GamePhase) extends GameStatus
 case class Won(winner: Player) extends GameStatus
