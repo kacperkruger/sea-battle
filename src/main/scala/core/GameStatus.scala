6b04cabb5cca21831bf7c2ea0745d0a7f2ffa24a
Kacper Kruger <kacper.kruger1@gmail.com>
2022-10-11 12:42:20 +0200

    added waiting for players status

--- src/main/scala/core/GameStatus.scala
+++ src/main/scala/core/GameStatus.scala
@@ -4,5 +4,6 @@ import core.GamePhase
 
 trait GameStatus
 
-case class OnGoing(board: Board, phase: GamePhase) extends GameStatus
+object WaitingForPlayers extends GameStatus
+case class OnGoing(nextPlayer: Player, phase: GamePhase) extends GameStatus
 case class Won(winner: Player) extends GameStatus
